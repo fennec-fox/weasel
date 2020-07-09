04f6fdaceecdc165efc7444a8471c9df07b115a5
fennec-fox <elijah17@gmail.com>
2020-07-09 13:36:35 +0900

    Refactor PayGateCustomException.kt

--- paygate/src/main/kotlin/io/mustelidae/weasel/paygate/config/PayGateCustomException.kt
+++ paygate/src/main/kotlin/io/mustelidae/weasel/paygate/config/PayGateCustomException.kt
@@ -2,10 +2,10 @@ package io.mustelidae.weasel.paygate.config
 
 import java.lang.RuntimeException
 
-class PayGateClientException(message: String, val canRetry: Boolean, val causeBy: Map<String, Any>? = null) : RuntimeException(message)
+open class PayGateException(message: String, open val causeBy: Map<String, Any>? = null) : RuntimeException(message)
+
+class PayGateClientException(message: String, val canRetry: Boolean, override val causeBy: Map<String, Any>? = null) : PayGateException(message, causeBy)
 
 class NotSupportPayMethodException(message: String) : RuntimeException(message)
-
-class PayGateException(message: String) : RuntimeException(message)
 
 class PayGateCertifyException(message: String) : RuntimeException(message)
