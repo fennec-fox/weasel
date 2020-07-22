d8e0e52e241851812d92030f6a9a61c22a554597
fennec-fox <elijah17@gmail.com>
2020-07-22 17:51:09 +0900

    Exclude dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,6 +2,13 @@ group = "io.mustelidae.weasel"
 
 dependencies {}
 
+configurations.forEach {
+    it.exclude("org.springframework")
+    it.exclude("org.springframework.boot")
+    it.exclude("ch.qos.logback")
+    it.exclude("org.slf4j")
+}
+
 tasks.getByName<Jar>("bootJar") {
     enabled = false
 }
